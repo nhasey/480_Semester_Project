126f5d26948f0fc6c701e3f101d36e2138d30a8d
Sammy Nouadir <180578838+Sammynouadir@users.noreply.github.com>
2024-11-30 19:50:09 -0500

    using larger dataset acc of 89% as well as more kernels

--- FinalCNN.py
+++ FinalCNN.py
@@ -47,12 +47,13 @@ test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=Fa
 class VerifierCNN(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 8, kernel_size=3, padding=1)
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
         self.relu = nn.ReLU()
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(8, 16, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(32 * 16 * 16, 2)  
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.conv4 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.fc1 = nn.Linear(256 * 8 * 8, 2)  
     
     def forward(self, x):
         x = self.relu(self.conv1(x))
@@ -60,6 +61,8 @@ class VerifierCNN(nn.Module):
         x = self.relu(self.conv2(x))
         x = self.pool(x)
         x = self.relu(self.conv3(x))
+        x = self.pool(x)
+        x = self.relu(self.conv4(x))
         x = self.pool(x)
         
         # Flatten dynamically
