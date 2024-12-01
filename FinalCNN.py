10dbed70049e89cad1d53ca6080a7cacf6182505
Sammy Nouadir <180578838+Sammynouadir@users.noreply.github.com>
2024-11-30 20:27:20 -0500

    lowered the amount of kernels

--- FinalCNN.py
+++ FinalCNN.py
@@ -4,7 +4,7 @@ import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
 import os
-from torch.utils.data import DataLoader, ConcatDataset, random_split
+from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
 
 # Setting up Kaggle API credentials
@@ -33,7 +33,7 @@ train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 test_dataset = ImageFolder(root=os.path.join(dataset_path, 'test'), transform=transform)
 test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
 
-print("still working 1")
+
 
 
 # Dataset loaders
@@ -44,13 +44,13 @@ test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=Fa
 class VerifierCNN(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
+        self.conv1 = nn.Conv2d(3, 8, kernel_size=3, padding=1)
         self.relu = nn.ReLU()
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-        self.conv4 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(256 * 8 * 8, 2)  
+        self.conv2 = nn.Conv2d(8, 16, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
+        self.conv4 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.fc1 = nn.Linear(64* 8 * 8, 2)  
     
     def forward(self, x):
         x = self.relu(self.conv1(x))
